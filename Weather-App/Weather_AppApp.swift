bc6939cb3f3495b6f9fb4f3e9fdf66303eb3fd52
masakachristopher <masakachristopher@yahoo.com>
2022-07-10 09:17:49 +0300

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Christopher Masaka on 10/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
